0222a20f1ad7982fd082c8cacaa640c9b1f1f4c0
chorny-znich <vargar.dt@gmail.com>
2024-09-26 16:55:57 +0300

    feat:

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -157,7 +157,7 @@ function insertItem(arr, item, index) {
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
 function getHead(arr, n) {
-  return arr.slice(n);
+  return arr.slice(0, n);
 }
 
 /**
@@ -170,8 +170,8 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(-n);
 }
 
 /**
@@ -194,8 +194,13 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.map((item, index) => {
+    result += item.join(',');
+    if (index !== arr.length - 1) {
+      result += '\n';
+    }
+  });
 }
 
 /**
--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -202,8 +202,12 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const start = (isStartIncluded) ? '[' : '(';
+  const end = (isEndIncluded) ? ']' : ')';
+  const minValue = Math.min(a, b);
+  const maxValue = Math.max(a, b);
+  return `${start}${minValue}, ${maxValue}${end}`;
 }
 
 /**
